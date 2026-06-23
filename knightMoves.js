1df8865b41a27f64e199bb15aa431064bf6203cc
AleixMJ <aleixtechIT@gmail.com>
2026-06-23 17:06:45 +0200

    created moves and queue and a pseudo code of next actions

--- knightMoves.js
+++ knightMoves.js
@@ -1,3 +1,13 @@
-function knightMoves(start, end) {
+function knightMoves(start, end, moves = 0, path = []) {
 
+    const moves = [
+        [2, 1], [-2, 1], [2, -1], [-2, -1],
+        [1, 2], [1, -2], [-1, 2], [-1, -2]
+    ];
+
+    const queue = [[start]];
+
+    // Add path of moves to the queue
+    // ensure a previous visited position within the path is not added again to a path.
+    // if a path reaches the end position return the path combination
 }
