10dbd441b62f9d745fb83a6b3dc8d18c1862eeae
AleixMJ <aleixtechIT@gmail.com>
2026-06-23 18:35:35 +0200

    WIP: working on the loop queue logic, need to review and add visited vertices to avoid repetition

--- knightMoves.js
+++ knightMoves.js
@@ -7,7 +7,27 @@ function knightMoves(start, end, moves = 0, path = []) {
 
     const queue = [[start]];
 
-    // Add path of moves to the queue
     // ensure a previous visited position within the path is not added again to a path.
+
+    const visited = new Set();
+    visited.add(start.toString());
+
+    // Add path of moves to the queue
     // if a path reaches the end position return the path combination
+
+    while (queue.length > 0) {
+
+        const currentPath = queue.shift();
+        for (move in moves) {   
+            const currentPathMove = currentPath.push(move);
+            
+            if (currentPathMove[currentPathMove.length - 1] === end) {
+                return currentPathMove;
+            }
+            queue.push(currentPathMove);        
+    }
+    
+    }
+    
+    
 }
